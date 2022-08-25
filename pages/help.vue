9923d4b2a5baa584e3d636e16f1216d14742f131
AMMAR <ammarbr@hotmail.com>
2022-08-25 09:29:02 +0200

    initial commit

---
+++ pages/help.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ $t('pages.help.title') }}</h1>
+    You can login with the following Account Data:
+    <p>
+      <b>User:</b> dehub-demo<br>
+      <b>Password:</b> demo
+    </p>
+    If you have any questions feel free to send an Email to: <a href="mailto:vengadeswaran@med.uni-frankfurt.de">vengadeswaran@med.uni-frankfurt.de</a>.
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
